b0b8d76df0418ed7eeac4a6bf7d5860757840ec9
jozaldua <johnamiel.zaldua@misys.com>
2015-07-23 20:21:04 +0800

    [Amiel][Rev 17] - Modify SelectMainActvity

--- app/src/main/java/com/misys/teamextrarice/mibudget/activities/SelectMainActivity.java
+++ app/src/main/java/com/misys/teamextrarice/mibudget/activities/SelectMainActivity.java
@@ -17,6 +17,7 @@ public class SelectMainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        System.out.print("Loading SelectMainActivity");
         selectMain();
     }
 
