43d8f44c8d0f69721707085ecacaa2bcd9d62f5e
jozaldua <johnamiel.zaldua@misys.com>
2015-07-23 18:11:43 +0800

    test

--- app/src/main/java/com/misys/teamextrarice/mibudget/activities/LoginActivity.java
+++ app/src/main/java/com/misys/teamextrarice/mibudget/activities/LoginActivity.java
@@ -3,6 +3,8 @@ package com.misys.teamextrarice.mibudget.activities;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
+import com.misys.teamextrarice.mibudget.R;
+
 /**
  * Created by jozaldua on 7/23/2015.
  */
@@ -11,6 +13,7 @@ public class LoginActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        System.out.print("test");
     }
 
 }
