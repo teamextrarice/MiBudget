21739fb314d990bec4d701650bbed823d0a6d2d8
jozaldua <johnamiel.zaldua@misys.com>
2015-07-23 22:45:39 +0800

    [Amiel][Rev 26] - Modify activity login xml

--- app/src/main/java/com/misys/teamextrarice/mibudget/activities/LoginActivity.java
+++ app/src/main/java/com/misys/teamextrarice/mibudget/activities/LoginActivity.java
@@ -79,36 +79,41 @@ public class LoginActivity extends ActionBarActivity {
 
     }
 
-
-
-    private class AsyncCallWS extends AsyncTask<String, Void, Void> {
-        @Override
-        protected Void doInBackground(String... params) {
-            login(findViewById(R.id.login_button));
-            //Log.i(TAG, "doInBackground");
-
-            //getFahrenheit(celcius);
-            //getAcctDetails(celcius);
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            //Log.i(TAG, "onPostExecute");
-            //tv.setText(fahren + "° F");
-
-        }
-
-        @Override
-        protected void onPreExecute() {
-            //Log.i(TAG, "onPreExecute");
-            //tv.setText("Calculating...");
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            //Log.i(TAG, "onProgressUpdate");
-        }
-
+    public void callRegisterScreen(View view){
+        Intent regIntent = new Intent(this,RegisterActivity.class);
+        regIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        getApplicationContext().startActivity(regIntent);
     }
+
+
+//    private class AsyncCallWS extends AsyncTask<String, Void, Void> {
+//        @Override
+//        protected Void doInBackground(String... params) {
+//            login(findViewById(R.id.login_button));
+//            //Log.i(TAG, "doInBackground");
+//
+//            //getFahrenheit(celcius);
+//            //getAcctDetails(celcius);
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Void result) {
+//            //Log.i(TAG, "onPostExecute");
+//            //tv.setText(fahren + "° F");
+//
+//        }
+//
+//        @Override
+//        protected void onPreExecute() {
+//            //Log.i(TAG, "onPreExecute");
+//            //tv.setText("Calculating...");
+//        }
+//
+//        @Override
+//        protected void onProgressUpdate(Void... values) {
+//            //Log.i(TAG, "onProgressUpdate");
+//        }
+//
+//    }
 }
---
+++ app/src/main/java/com/misys/teamextrarice/mibudget/activities/RegisterActivity.java
@@ -0,0 +1,44 @@
+package com.misys.teamextrarice.mibudget.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.EditText;
+
+import com.misys.teamextrarice.mibudget.R;
+import com.misys.teamextrarice.mibudget.database.MyDB;
+
+import java.util.Date;
+
+/**
+ * Created by jozaldua on 7/23/2015.
+ */
+public class RegisterActivity extends ActionBarActivity {
+    private EditText name;
+    private EditText password;
+    private EditText acctnum;
+    private Date cutOffDate;
+    private Date birthDate;
+    private String job;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_screen);
+        setupGUI();
+    }
+
+    private void setupGUI(){
+//        name = (EditText)findViewById(R.id.name_input);
+//        password = (EditText)findViewById(R.id.pw_input);
+//        acctnum = (EditText)findViewById(R.id.acctnum_input);
+//        cutOffDate = (EditText)findViewById(R.id.cutoffdate_input);
+//        birthDate = (EditText)findViewById(R.id.birthdate_input);
+//        job = (EditText)findViewById(R.id.job_input);
+    }
+
+    public void createRecord(){
+        MyDB db = MyDB.getInstance();
+        db.setdbHelper(this);
+        //db.createRecord(name.getText().toString(),password.getText().toString(),acctnum.getText(),cutOffDate.get);
+    }
+}
